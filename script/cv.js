5023a14edb5dbc43a6707f13f44a15eb10f8330a
Raphaël KIECKEN <raphael.kiecken.etu@univ-lille.fr>
2023-12-04 20:34:52 +0100

    Big fix

--- script/cv.js
+++ script/cv.js
@@ -1,42 +1,21 @@
 let currentContent;
-let currentProfile = 0;
 let baseProfileTextHeight;
+
+window.onload = init;
+
+function init() {
+  const content = document.getElementsByClassName('content');
+  for (let i = 0; i < content.length; i++) {
+    content[i].style.opacity = 0;
+  }
+  changeContent(0);
+}
 
 function changeContent(id) {
   const content = document.getElementsByClassName('content');
-  const profilePic = document.getElementsByClassName('profile-pic');
-  const profileText = document.getElementsByClassName('profile-text');
   if (currentContent === id) return;
   if (currentContent === undefined) currentContent = id;
-  content[currentContent].style.display = 'none';
-  content[id].style.display = 'flex';
+  content[currentContent].style.opacity = 0;
+  content[id].style.opacity = 1;
   currentContent = id;
-  if (currentContent === 0) {
-    currentProfile = 0;
-    profilePic[currentProfile].style.opacity = '1';
-    profileText[currentProfile].style.opacity = '1';
-    profilePic[(currentProfile + 1) % 2].style.opacity = '0';
-    profileText[(currentProfile + 1) % 2].style.opacity = '0';
-    const profileTextContainer = document.getElementById('profile-text-container');
-    baseProfileTextHeight = profileTextContainer.offsetHeight;
-    profileTextContainer.style.height = baseProfileTextHeight + profileText[currentProfile].offsetHeight + 'px';
-  }
-}
-
-function changeProfile() {
-  const profileTextContainer = document.getElementById('profile-text-container');
-  const profilePic = document.getElementsByClassName('profile-pic');
-  const profileText = document.getElementsByClassName('profile-text');
-
-  profilePic[currentProfile].style.opacity = '0';
-  profileText[currentProfile].style.opacity = '0';
-
-  currentProfile = (currentProfile + 1) % 2;
-
-  profilePic[currentProfile].style.opacity = '1';
-  profileText[currentProfile].style.opacity = '1';
-
-
-
-  profileTextContainer.style.height = baseProfileTextHeight + profileText[currentProfile].offsetHeight + 'px';
 }
